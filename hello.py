97d591f4c67164ebe851fb50c67cfb08f72f2f13
rfslf <stanislav@avdonin.me>
2016-05-15 15:07:34 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 def wcgi_app(environ, start_responce):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    
-    
-    
+    body=environ['QUERY_STRING'].split('&')
+    body = [item+'\r\n' for item in body]
     start_response(status, headers)
-    return [body]
+    return body
