77a7f26f52f65aaa14044efdcb4b2ef2a42ba27a
rfslf <stanislav@avdonin.me>
2016-05-19 17:10:14 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,14 +3,19 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
-    text = models.TextField
-    added_at = models.DateTimeField
-    rating = models.IntegerField
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set') 
+    class Meta:
+        db_table = 'question'
+        
 class Answer(models.Model):
-    text = models.TextField
-    added_at = models.DateTimeField
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.OneToOneField(AnswerQuestion)
+#    question = ForeignKey(Question)
     author = models.ForeignKey(User)
-# Create your models here.
+    class Meta:
+        db_table = 'answer'
