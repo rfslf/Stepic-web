d52bcd1f70c26b7b0790b9384192091f76ed4876
rfslf <stanislav@avdonin.me>
2016-05-19 16:56:15 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField
+    added_at = models.DateTimeField
+    rating = models.IntegerField
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='likes_set') 
+class Answer(models.Model):
+    text = models.TextField
+    added_at = models.DateTimeField
+    question = models.OneToOneField(AnswerQuestion)
+    author = models.ForeignKey(User)
 # Create your models here.
