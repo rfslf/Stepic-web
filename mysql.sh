a441535efb6233d81f7f26ebd10382da03fbe5e2
rfslf <stanislav@avdonin.me>
2016-05-20 11:58:27 +0300

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,3 @@
 mysql -uroot -e "CREATE DATABASE askweb;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON askweb.* TO 'box'@'localhost' WITH GRANT OPTION;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON askweb.* TO 'x'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'box'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -e "FLUSH PRIVILEGES;"
