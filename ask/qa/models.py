fc4b88addac95d4c20737ba00da6ba7d1fccb943
rfslf <stanislav@avdonin.me>
2016-05-20 13:16:47 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,7 +10,7 @@ class QuestionManager():
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set') 
@@ -20,7 +20,7 @@ class Question(models.Model):
         
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateField(auto_now_add=True)
 #    question = models.OneToOneField(AnswerQuestion)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
