b48245c0351536afc9fc8b499dbd19f71caeec71
rfslf <stanislav@avdonin.me>
2016-05-21 17:59:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ sudo /etc/init.d/nginx restart
 #sudo /etc/init.d/mysql restart
 sudo rm /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#mysql -uroot -e "CREATE DATABASE db_stepic_project;"
+#mysql -uroot -e "CREATE DATABASE askweb;"
 #mysql -uroot -e "CREATE USER 'django'@'localhost' IDENTIFIED BY 'django';"
 #mysql -uroot -e "GRANT ALL PRIVILEGES ON db_stepic_project.* TO 'django'@'localhost';"
 #mysql -uroot -e "FLUSH PRIVILEGES;"
 #/home/box/web/ask/manage.py syncdb
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart
