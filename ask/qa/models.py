73a8bc8eeb8476218aa37458a349d26ef8d2fc80
rfslf <stanislav@avdonin.me>
2016-05-22 00:39:16 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -27,3 +27,18 @@ class Answer(models.Model):
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User, default=1)
+
+class AskForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    text = forms.CharField(widget=forms.Textarea)
+    hidden = forms.CharField(widget=forms.HiddenInput())
+    def clean(self):
+        text = self.cleaned_data['text']
+        if not text.is_valid():
+            raise forms.ValidationError('question text is wrong', code=12)
+        return text
+    def save(self):
+        question = Question(**self.cleaned_data)
+        question.author_id = 1
+        question.save()
+        return question
