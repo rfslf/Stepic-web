e9609df833f2b8034917de323236e05cd27e9e3f
rfslf <stanislav@avdonin.me>
2016-05-21 15:28:24 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,20 +12,18 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
-#    added_at = models.DateField()
+#    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, default=1)
     likes = models.ManyToManyField(User, related_name='likes_set') 
-#    objects = QuestionManager()
-#    class Meta:
-#        db_table = 'question'
-        
+    def __unicode__(self):
+        return self.title
+    def get_abs_url(self):
+        return reverse('question', kwargs={"id": self.id})
+      
 class Answer(models.Model):
     text = models.TextField()
-#    added_at = models.DateField(auto_now_add=True)
-    added_at = models.DateField()
-#    question = models.OneToOneField(AnswerQuestion)
+#    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
-#   class Meta:
-#       db_table = 'answer'
+    author = models.ForeignKey(User, default=1)
