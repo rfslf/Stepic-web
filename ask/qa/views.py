48d48928d50bf2a7858bac4bf5e43ab2164eb760
rfslf <stanislav@avdonin.me>
2016-05-21 15:56:33 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
 # Create your views here.
+def allq(request):
+    
 
+def show_question(request, q_id):
+    try:
+        question = Question.objects.get(id = q_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'ask/question.html', {
+        'question' : question,
+        'title' : question.title,
+        'text' : question.text,
+         })
