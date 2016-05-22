1eeb7d5cbd8307304fabacf8978e0e3a92734c8e
rfslf <stanislav@avdonin.me>
2016-05-23 01:41:29 +0300

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,6 @@
+sudo service mysql start
 mysql -uroot -e "CREATE DATABASE askweb;"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'box'@'localhost' WITH GRANT OPTION;"
 mysql -uroot -e "FLUSH PRIVILEGES;"
+cd ask/
+python manage.py validate
